1e42f1f26dd59f82ba74719f0dfd6301924b6b8a
Ayres Roux <devayresrouxj@gmail.com>
2025-12-05 16:48:06 -0300

    aula: herança, polimorfismo e encapsulamento - parte 2

--- pilares_poo.py
+++ pilares_poo.py
@@ -1,3 +1,4 @@
+print("\nExemplo de Herença")
 class Animal:
   def __init__(self, nome):
     self.nome = nome
@@ -16,3 +17,35 @@ class Cachorro(Animal):
 class Gato(Animal):
   def emitir_som(self):
     return "miau"
+  
+dog = Cachorro(nome="Rex")
+cat = Gato(nome="Felix")
+
+print("\nExemplo de polimorfismo")
+animais = [dog, cat]
+
+for animal in animais:
+  print(f"{animal.nome} faz: {animal.emitir_som()}")
+
+print("\nExemplo de Encapsulamento")
+class ContaBancaria:
+  def __init__(self, saldo):
+    self.__saldo = saldo # Atributo privado
+  
+  def depositar(self, valor):
+    if valor > 0:
+      self.__saldo += valor
+  
+  def sacar(self, valor):
+    if valor > 0 and valor <= self.__saldo:
+      self.__saldo -= valor
+
+  def consultar_saldo(self):
+    return self.__saldo
+  
+conta = ContaBancaria(saldo=1000)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
+conta.depositar(500)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
+conta.sacar(700)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
